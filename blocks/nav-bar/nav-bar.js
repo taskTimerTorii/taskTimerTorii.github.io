6db18f3d68452fd54a4500d78f880f5e0f294902
Torii Academy <TaskTimerTorii@gmail.com>
2017-03-10 15:02:29 +0100

    play-pause nav-bar, border search-bar, zlikwidowany focus w nav-bar

--- blocks/nav-bar/nav-bar.js
+++ blocks/nav-bar/nav-bar.js
@@ -8,3 +8,25 @@
         cross.classList.toggle('top-bar__cross_hidden');
         console.log('navbar on/off');
     });
+
+(function() {
+    var play = document.getElementsByClassName('nav-bar__play');
+    var pause = document.getElementsByClassName('nav-bar__pause');
+    var playPause =document.getElementsByClassName('nav-bar__play-pause');
+    
+    for (var i = 0, max = play.length; i < max; i++){
+        playPause[i].addEventListener('click', function(event) {
+            if (event.target.classList.contains('nav-bar__play-pause')){
+                console.log('nav-bar__play-pause');
+            }   else {
+                console.log(event.target);
+                event.target.classList.toggle('nav-bar__pause_visible');
+                event.target.classList.toggle('nav-bar__pause_hidden');
+                event.target.previousElementSibling.classList.toggle('nav-bar__play_hidden');
+                event.target.previousElementSibling.classList.toggle('nav-bar__play_visible');
+            }
+        });
+    }
+        
+        
+}());
