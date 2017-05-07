3c9f91b03b74e5ae8ecdbafcdc6288dcff632b45
Tomek <tomekkleszczewski@gmail.com>
2017-05-07 22:50:23 +0200

    projects

---
+++ Projects/projects.js
@@ -0,0 +1,15 @@
+(function(){
+    var projects = document.getElementsByClassName('projects')[0];
+    var hamburger = document.getElementsByClassName('top-bar__hamburger')[0];
+    var arrows = document.getElementsByClassName('project__arrow-down');
+    var max = arrows.length;
+    for (var i = 0; i < max; i ++){
+        arrows[i].addEventListener('click', function(event){
+            event.target.parentNode.classList.toggle('project_open');
+            event.target.classList.toggle('project__arrow-up');
+        });
+    }
+    hamburger.addEventListener('click', function() {
+        projects.classList.toggle('projects_dark');
+    });
+}());
